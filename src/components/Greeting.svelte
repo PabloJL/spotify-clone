9bc2719a9e6da4b33dd0fad987015c38eacc73c7
PabloJL <jup_jim_7@hotmail.com>
2024-04-15 17:35:38 -0600

    Audio Added

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    let greeting = ""
+
+    if(currentHour<12){
+        greeting = "Good Morning"
+    }else if(currentHour <18){
+        greeting = "Good Afternoon"
+    }else{
+        greeting = "Good Evening"
+    }
+</script>
+
+<h1 class="text-3xl font-bold text-white">
+    {greeting}
+</h1>
