d00505e848896c5e1bedbc7364547d75942580f2
PabloJL <jup_jim_7@hotmail.com>
2024-04-17 17:40:44 -0600

    Music list completed

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,6 +1,6 @@
 import { Pause, Play } from "./Player";
 import { usePlayerStore } from "@/store/playerStore";
-export default function CardPlayButton({ id }) {
+export default function CardPlayButton({ id, size = "small" }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
@@ -20,12 +20,17 @@ export default function CardPlayButton({ id }) {
       });
   };
 
+  const iconClassName = size === "small" ? "w-4 h-4" : "w-5 h-5";
   return (
     <button
       onClick={handleClick}
-      className="card-play-button rounded-full bg-green-500 p-4"
+      className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400"
     >
-      {isPlaylistPlaying ? <Pause /> : <Play />}
+      {isPlaylistPlaying ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
     </button>
   );
 }
