be2509654cb32d26754e6a1dd13949f4d9b1073a
PabloJL <jup_jim_7@hotmail.com>
2024-04-18 17:18:46 -0600

    Optimizing play button

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -4,20 +4,24 @@ export default function CardPlayButton({ id, size = "small" }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
-  const isPlaylistPlaying = isPlaying && currentMusic?.playlist.id === id;
+  const isPlaylistPlaying = isPlaying && currentMusic?.playlist?.id === id;
+  const currentMusicPlaying = currentMusic?.playlist?.id === id;
   const handleClick = () => {
     if (isPlaylistPlaying) {
       setIsPlaying(false);
       return;
+    } else if (currentMusicPlaying) {
+      setIsPlaying(true);
+      return;
+    } else {
+      fetch(`/api/get-info-playlist.json?id=${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          const { songs, playlist } = data;
+          setIsPlaying(true);
+          setCurrentMusic({ songs, playlist, song: songs[0] });
+        });
     }
-
-    fetch(`/api/get-info-playlist.json?id=${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        const { songs, playlist } = data;
-        setIsPlaying(true);
-        setCurrentMusic({ songs, playlist, song: songs[0] });
-      });
   };
 
   const iconClassName = size === "small" ? "w-4 h-4" : "w-5 h-5";
