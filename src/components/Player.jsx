4b3c00f1574db175dfcf91ddf7a03b4c19f1b17c
PabloJL <jup_jim_7@hotmail.com>
2024-04-18 18:26:07 -0600

    Next and previous controls

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,13 @@
 import { useEffect, useRef } from "react";
 import { usePlayerStore } from "@/store/playerStore";
-import { Pause, Play } from "@/icons/Controls";
+import { Pause, Play, Next, Previous } from "@/icons/Controls";
 import { VolumeControl } from "./Controls/VolumeControl";
 import { SongControl } from "./Controls/SongControl";
 import { CurrentSong } from "./Controls/CurrentSong";
 
 export default function Player() {
-  const { isPlaying, setIsPlaying, currentMusic, volume } = usePlayerStore(
-    (state) => state
-  );
+  const { isPlaying, setIsPlaying, setCurrentMusic, currentMusic, volume } =
+    usePlayerStore((state) => state);
 
   const audioRef = useRef();
 
@@ -34,6 +33,29 @@ export default function Player() {
     setIsPlaying(!isPlaying);
   };
 
+  const handleNextClick = () => {
+    const { song, songs, playlist } = currentMusic;
+    const nextSongIndex = songs.findIndex((s) => s.id === song.id) + 1;
+    const nextSong = nextSongIndex < songs.length ? songs[nextSongIndex] : null;
+
+    if (nextSong) {
+      setCurrentMusic({ songs, playlist, song: nextSong });
+    } else {
+      setCurrentMusic({ songs, playlist, song: songs[0] });
+    }
+  };
+
+  const handlePreviousClick = () => {
+    const { song, songs, playlist } = currentMusic;
+    const prevSongIndex = songs.findIndex((s) => s.id === song.id) - 1;
+    const prevSong = prevSongIndex > 0 ? songs[prevSongIndex] : null;
+
+    if (prevSong) {
+      setCurrentMusic({ songs, playlist, song: prevSong });
+    } else {
+      setCurrentMusic({ songs, playlist, song: songs[0] });
+    }
+  };
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50 text-white">
       <div className="w-[350px]">
@@ -41,9 +63,18 @@ export default function Player() {
       </div>
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center flex-col items-center">
-          <button className="bg-white rounded-full p-2" onClick={handleClick}>
-            {isPlaying ? <Pause /> : <Play />}
-          </button>
+          <div className="flex justify-between gap-5">
+            <button className="p-2" onClick={handlePreviousClick}>
+              <Previous />
+            </button>
+            <button className="bg-white rounded-full p-2" onClick={handleClick}>
+              {isPlaying ? <Pause /> : <Play />}
+            </button>
+
+            <button className=" p-2" onClick={handleNextClick}>
+              <Next />
+            </button>
+          </div>
           <SongControl audio={audioRef} />
           <audio ref={audioRef} />
         </div>
